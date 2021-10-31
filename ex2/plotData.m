0a15b022252564c8cd2b54da7947ef0e285cc9be
Sueda Ciftci <44266522+edasu@users.noreply.github.com>
2021-10-31 13:02:22 +0000

    Update plotData.m

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -10,22 +10,6 @@ pos = find(y==1); neg = find(y == 0);
 % Plot Examples
 plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
 plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y','MarkerSize', 7);
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
 
 
 hold off;
