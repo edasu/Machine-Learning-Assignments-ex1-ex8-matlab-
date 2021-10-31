bbb3dc0e55d22027b955817e9aef97dce8bf1619
Sueda Ciftci <44266522+edasu@users.noreply.github.com>
2021-10-31 13:15:08 +0000

    Update normalEqn.m

--- ex1/normalEqn.m
+++ ex1/normalEqn.m
@@ -5,19 +5,5 @@ function [theta] = normalEqn(X, y)
 
 theta = inv(X.' * X) * X.' * y;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the code to compute the closed form solution
-%               to linear regression and put the result in theta.
-%
-
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-% -------------------------------------------------------------
-
-
-% ============================================================
 
 end
