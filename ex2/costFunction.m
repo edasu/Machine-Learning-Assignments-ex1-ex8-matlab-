d11c930710d7eb27051965c951acf2a593267032
Sueda Ciftci <44266522+edasu@users.noreply.github.com>
2021-10-31 13:01:50 +0000

    Update costFunction.m

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -13,22 +13,4 @@ J = (1 / m) * sum(-y'* log(H) - (1 - y)'* log(1 - H));
 grad = (1 / m) * (X' *(H - y));
 grad = grad(:);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
-
 end
